95b2f61453754e3e25ba7311d979f5a1d9fc6ae7
Rinrin.rs <rinrin0413.rs@gmail.com>
2025-11-26 11:08:25 +0900

    experim: 全スタイルシートを`style`タグに直接埋め込むよう設定（試験的）[#15]
    ## 懸念点
    
    - HTML サイズが増加するため、View Transition に悪影響が出る可能性
    - CSS ファイル分キャッシュ効率が下がる。

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+
   vite: {
     css: {
       preprocessorOptions: {
