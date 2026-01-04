f7852b4faf612f0b667ab4a4f5abbe00659bc947
RetoRuto9900K <tutinoko2048@gmail.com>
2026-01-04 23:56:21 +0900

    refactor: `pnpm-license`を使ったライセンス情報生成に変更, eslintルールの更新

---
+++ scripts/collect-licenses.js
@@ -0,0 +1,68 @@
+//@ts-check
+import * as fs from "fs/promises";
+import { getDependencies, getLicenseText } from "@quantco/pnpm-licenses";
+
+/** @type {string[]} */
+const ALLOWED_LICENSES = [
+  "0BSD",
+  "Apache-2.0",
+  "Apache-2.0 AND LGPL-3.0-or-later",
+  "Apache-2.0 AND MIT",
+  "BSD",
+  "BSD-2-Clause",
+  "BSD-3-Clause",
+  "BSD-4-Clause",
+  "CC0-1.0",
+  "ISC",
+  "LGPL-3.0-or-later",
+  "MIT",
+  "MIT OR CC0-1.0",
+  "OFL-1.1",
+  "Python-2.0",
+];
+
+const dependencies = await getDependencies(
+  { prod: true },
+  {
+    stdin: false,
+    stdout: false,
+    inputFile: undefined,
+    outputFile: "",
+  },
+);
+
+/** @type {import('../src/types').Dependency[]} */
+const output = [];
+for (const dependency of dependencies) {
+  if (!isAllowedLicense(dependency.license)) {
+    console.error(
+      `Disallowed license found: "${dependency.license}" (${dependency.name}@${dependency.version})`,
+    );
+    process.exit(1);
+  }
+  const { licenseText } = await getLicenseText(dependency);
+  output.push({
+    name: dependency.name,
+    version: dependency.version,
+    license: dependency.license,
+    homepage: dependency.homepage,
+    licenseText,
+  });
+}
+
+output.sort((a, b) => a.version.localeCompare(b.version));
+output.sort((a, b) => a.name.localeCompare(b.name));
+
+await fs.writeFile("licenses.json", JSON.stringify(output, null, 2));
+
+console.log("Successfully generated licenses.json!");
+
+/**
+ * @param {string} license
+ * @returns {boolean}
+ */
+function isAllowedLicense(license) {
+  return ALLOWED_LICENSES.some(
+    (allowed) => license === allowed || license === `(${allowed})`,
+  );
+}
